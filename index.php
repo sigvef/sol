c1ca588ca6b09fe62a43166037eae0c71edacadc
Iver Jordal <iver56@hotmail.com>
2012-07-14 23:14:37 +0200

    minified script

--- index.php
+++ index.php
@@ -1,7 +1,2 @@
-<?php
-if (stristr($_SERVER['HTTP_USER_AGENT'], 'chrome') != false) {
-	header('Location: HONEYCOMB.html') ;
-}else{
-	echo "This demo works in chrome only";
-}
-?>
+<?php if(stristr($_SERVER['HTTP_USER_AGENT'],'chrome'))	header('Location: HONEYCOMB.html');
+else echo "This demo works in Chrome only";?>
